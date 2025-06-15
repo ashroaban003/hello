d2e17552b9bf0900b9972c6e537a1bec195730ab
ashroaban003 <ratnakumar48070@gmail.com>
2025-06-15 19:57:08 +0530

    final

--- src/main/java/com/ashishcodes/hello/OrderService.java
+++ src/main/java/com/ashishcodes/hello/OrderService.java
@@ -1,9 +1,16 @@
 package com.ashishcodes.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderService {
-    public void placeOrder(){
+    @RequestMapping("/order")
+    public String placeOrder(){
         var paymentservice = new Stripeservice();
         paymentservice.processPayment(10.0);
+        return "FF";
         //tightly couble with stripservice
         //we use interface to decouple them
     }
